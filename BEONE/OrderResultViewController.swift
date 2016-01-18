c582401c7f442476dcc87f491f4b1c1bdf99e08a
Join <choin1116@gmail.com>
2016-01-18 11:24:14 +0900

    temp

--- BEONE/OrderResultViewController.swift
+++ BEONE/OrderResultViewController.swift
@@ -3,5 +3,24 @@ import UIKit
 
 class OrderResultViewController: BaseViewController {
 
+  @IBOutlet weak var resultImageView: UIImageView!
+  @IBOutlet weak var resultView: UIView!
   var orderResult: [String: AnyObject]?
+  var paymentInfo = PaymentInfo()
+  
+  override func setUpData() {
+    super.setUpData()
+    paymentInfo.id = Int(orderResult?["paymentInfoId"] as! String)
+    paymentInfo.orderId = orderResult?["orderId"] as? Int
+    paymentInfo.get { () -> Void in
+      self.configureResultView()
+    }
+  }
+  
+  func configureResultView() {
+    resultView.subviews.forEach { $0.removeFromSuperview() }
+    let paymentInfoViewNibName: String
+    
+//    let paymentInfoView =
+  }
 }
--- BEONE/PaymentInfo.swift
+++ BEONE/PaymentInfo.swift
@@ -12,6 +12,7 @@ class PaymentInfo: BaseModel {
   var isMainPayment = false
   var paymentStatus: String?
   var title: String?
+  var orderId: Int?
   
   var bankName: String?
   var issureName: String?
@@ -21,23 +22,31 @@ class PaymentInfo: BaseModel {
   var expiredAt: NSDate?
   var paidAt: NSDate?
   
+  override func fetchUrl() -> String {
+    return "users/\(MyInfo.sharedMyInfo().userId!)/orders/\(orderId!)/payment-infos/\(id!)"
+  }
+  
   override func assignObject(data: AnyObject) {
-    if let paymentInfo = data as? [String: AnyObject] {
-      id = paymentInfo[kObjectPropertyKeyId] as? Int
-
-      if let price = paymentInfo["actualPrice"] as? Int {
-        self.price = price
+    if let data = data as? [String: AnyObject] {
+      let paymentInfoObejct = data[kNetworkResponseKeyData] != nil ? data[kNetworkResponseKeyData] : data
+      if let paymentInfo = paymentInfoObejct as? [String: AnyObject] {
+        print(data)
+        id = paymentInfo[kObjectPropertyKeyId] as? Int
+        
+        if let price = paymentInfo["actualPrice"] as? Int {
+          self.price = price
+        }
+        if let currencyTypeString = paymentInfo["currencyType"] as? String,
+          currencyType = CurrencyType(rawValue: currencyTypeString) {
+            self.currencyType = currencyType
+        }
+        if let isMainPayment = paymentInfo["isMainPayment"] as? Bool {
+          self.isMainPayment = isMainPayment
+        }
+        
+        paymentStatus = paymentInfo["paymentStatus"] as? String
+        title = paymentInfo["title"] as? String
       }
-      if let currencyTypeString = paymentInfo["currencyType"] as? String,
-        currencyType = CurrencyType(rawValue: currencyTypeString) {
-          self.currencyType = currencyType
-      }
-      if let isMainPayment = paymentInfo["isMainPayment"] as? Bool {
-        self.isMainPayment = isMainPayment
-      }
-      
-      paymentStatus = paymentInfo["paymentStatus"] as? String
-      title = paymentInfo["title"] as? String
     }
   }
 }
