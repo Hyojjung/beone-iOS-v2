a55b28f680d93cc574ea7861b11e5d257542ee8a
hyojjung <khyojung0817@gmail.com>
2016-04-14 01:42:53 +0900

    modifying button template

--- BEONE/ButtonTemplateCell.swift
+++ BEONE/ButtonTemplateCell.swift
@@ -5,29 +5,47 @@ import SDWebImage
 class ButtonTemplateCell: TemplateCell {
   
   @IBOutlet weak var button: UIButton!
-  var unpressedBackgroundColor: UIColor?
-  var pressedBackgroundColor: UIColor?
   
   override func configureCell(template: Template) {
     super.configureCell(template)
+    
     button.setTitle(template.content.text, forState: .Normal)
+    button.setTitle(template.content.text, forState: .Highlighted)
+    
     button.setTitleColor(template.content.textColor, forState: .Normal)
-    button.setTitleColor(template.content.pressedTextColor, forState: .Highlighted)
+    if let pressedTextColor = template.content.pressedTextColor {
+      button.setTitleColor(pressedTextColor, forState: .Highlighted)
+    } else {
+      button.setTitleColor(template.content.textColor, forState: .Highlighted)
+    }
+    
     if let textSize = template.content.textSize {
       button.titleLabel?.font = UIFont.systemFontOfSize(textSize)
     }
-    button.layoutMargins = template.content.padding
+    
+    button.contentEdgeInsets = template.content.padding
     button.backgroundColor = template.content.backgroundColor
-    unpressedBackgroundColor = template.content.backgroundColor
-    pressedBackgroundColor = template.content.pressedBackgroundColor
+    
+    if template.content.backgroundColor != nil && template.content.backgroundImageUrl == nil {
+      let backgroundColorImage = template.content.backgroundColor?.imageWithColor()
+      button.setBackgroundImage(backgroundColorImage, forState: .Normal)
+    } else if let backgroundImageUrl = template.content.backgroundImageUrl {
+      button.sd_setBackgroundImageWithURL(backgroundImageUrl.url(), forState: .Normal)
+    } else {
+      button.backgroundColor = UIColor.clearColor()
+    }
+    if template.content.pressedBackgroundColor != nil && template.content.pressedBackgroundImageUrl == nil {
+      let pressedBackgroundColorImage = template.content.pressedBackgroundColor?.imageWithColor()
+      button.setBackgroundImage(pressedBackgroundColorImage, forState: .Highlighted)
+    } else if let pressedBackgroundImageUrl = template.content.pressedBackgroundImageUrl {
+      button.sd_setBackgroundImageWithURL(pressedBackgroundImageUrl.url(), forState: .Normal)
+    }
     
     if let borderColor = template.content.borderColor {
       button.layer.borderColor = borderColor.CGColor
       button.layer.borderWidth = 1
     }
     
-    button.sd_setBackgroundImageWithURL(template.content.backgroundImageUrl?.url(), forState: .Normal)
-    button.sd_setBackgroundImageWithURL(template.content.pressedBackgroundImageUrl?.url(), forState: .Highlighted)
     templateId = template.id
   }
   
@@ -43,8 +61,8 @@ class ButtonTemplateCell: TemplateCell {
       if let textSize = template.content.textSize {
         button.titleLabel?.font = UIFont.systemFontOfSize(textSize)
       }
-      button.layoutMargins = template.content.padding
-      if template.content.borderColor != nil{
+      button.contentEdgeInsets = template.content.padding
+      if template.content.borderColor != nil {
         button.layer.borderWidth = 1
       }
       button.sizeToFit()
@@ -55,14 +73,9 @@ class ButtonTemplateCell: TemplateCell {
     }
   }
   
-  @IBAction func buttonClicked(sender: AnyObject) { //Touch Up Inside action
-    button.backgroundColor = unpressedBackgroundColor
+  @IBAction func buttonClicked(sender: AnyObject) {
     action?.action()
   }
   
-  @IBAction func buttonReleased(sender: AnyObject) { //Touch Down action
-    if pressedBackgroundColor != nil {
-      button.backgroundColor = pressedBackgroundColor
-    }
-  }
+
 }
