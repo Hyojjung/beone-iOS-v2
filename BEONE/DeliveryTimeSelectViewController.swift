a79bbde19bcdaf74b28f4425269e5ddbe3baff40
hyojjung <khyojung0817@gmail.com>
2016-04-14 18:14:04 +0900

    time select view height

--- BEONE/DeliveryTimeSelectViewController.swift
+++ BEONE/DeliveryTimeSelectViewController.swift
@@ -3,25 +3,39 @@ import UIKit
 
 class DeliveryTimeSelectViewController: UIViewController {
   
+  private let kTimeSelectScrollViewTopInterval: CGFloat = 80
+  private let kTimeSelectScrollViewBottomInterval: CGFloat = 25
+  private let kTimeViewVerticalInterval: CGFloat = 25
+  
   @IBOutlet weak var timeSelectScrollView: UIScrollView!
-  let timeSelectView = TimeSelectView()
+  @IBOutlet weak var timeSelectView: TimeSelectView!
+  @IBOutlet weak var bottomLayoutConstraint: NSLayoutConstraint!
+  @IBOutlet weak var topLayoutConstraint: NSLayoutConstraint!
+  
   var availableTimeRanges: [AvailableTimeRange]?
   var selectedTimeRange: AvailableTimeRange?
-  weak var delegate: TimeSelectViewDelegate? {
-    didSet {
-      timeSelectView.delegate = delegate
-    }
-  }
-  
+  weak var delegate: TimeSelectViewDelegate?
+
   override func viewDidLoad() {
     super.viewDidLoad()
-    timeSelectScrollView.addSubViewAndEdgeLayout(timeSelectView)
-    timeSelectScrollView.addEqualWidthLayout(timeSelectScrollView, view2: timeSelectView)
-    timeSelectScrollView.addEqualHeightLayout(timeSelectScrollView, view2: timeSelectView)
+    timeSelectView.delegate = delegate
   }
   
   override func viewWillAppear(animated: Bool) {
     super.viewWillAppear(animated)
     timeSelectView.layoutView(availableTimeRanges!, selectedTimeRange: selectedTimeRange)
+    timeSelectView.setNeedsDisplay()
+    timeSelectView.layoutIfNeeded()
+
+    let margin = ViewControllerHelper.screenHeight - timeSelectView.frame.height -
+      kTimeSelectScrollViewTopInterval - kTimeSelectScrollViewBottomInterval
+    
+    if margin > kTimeViewVerticalInterval * 2 {
+      bottomLayoutConstraint.constant = margin / 2
+      topLayoutConstraint.constant = margin / 2
+    } else {
+      bottomLayoutConstraint.constant = kTimeViewVerticalInterval
+      bottomLayoutConstraint.constant = kTimeViewVerticalInterval
+    }
   }
 }
